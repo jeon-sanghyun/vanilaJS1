84531a42fb0f649cff773b84f7e27d11dad14150
sang hyun Jeon <jsh0813@gmail.com>
2020-05-08 16:09:37 +0900

    -index.js data type

--- index.js
+++ index.js
@@ -2,7 +2,27 @@
 
 // console.log('Im working.Im JS. Im Beautiful. Im worth it');
 
-let a = 221;
-let b = a-5;
-a=4;
-console.log(b,a);
+// let a = 221;
+// let b = a-5;
+// a=4;
+// console.log(b,a);
+
+// //String
+// const what = "Nicolas";
+// console.log(what);
+
+// //Boolean
+// const what = "Nicolas";
+// console.log(what);
+
+// //number
+// const what = 234235;
+// console.log(what);
+
+// //float
+// const what = 234.386758;
+// console.log(what);
+
+// //String
+// const what = "Nicolas";
+// console.log(what);
