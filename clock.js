2b5fe85a7ba0a6960a2ae372e9118655f774623b
sang hyun Jeon <jsh0813@gmail.com>
2020-05-08 13:01:41 +0900

    kk

---
+++ clock.js
@@ -0,0 +1,12 @@
+// const clockContainer = document.querySelector(".js-clock");
+// const clockTitle = clockContainer.querySelector("h1");
+
+// function gettime(){
+//     const date = new Date();
+// }
+
+// function init(){
+
+// }
+
+// init();
