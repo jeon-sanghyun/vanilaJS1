0dafa73496046bd5f2ce15dbb56a7cd7757d5578
sang hyun Jeon <jsh0813@gmail.com>
2020-05-08 15:19:17 +0900

    -index.js variable

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // alert('Im working.Im JS. Im Beautiful. Im worth it');
 
-console.log('Im working.Im JS. Im Beautiful. Im worth it');
+// console.log('Im working.Im JS. Im Beautiful. Im worth it');
+
+let a = 221;
+b = a-5;
+console.log(b);
