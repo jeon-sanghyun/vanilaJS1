50cb1f385f250b3b9e78dbbdeded3edbb6fa08d3
sang hyun Jeon <jsh0813@gmail.com>
2020-05-12 00:31:25 +0900

    - todo.js foreach, JSON, array, push, new

--- todo.js
+++ todo.js
@@ -4,15 +4,29 @@ const toDoForm= document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'todos'
 
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintTodo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText ="❌";
     const span = document.createElement("span");
+    const newId = toDos.length +1
+    delBtn.innerText ="❌";
     span.innerText = text
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj ={
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event){
@@ -22,14 +36,19 @@ function handleSubmit(event){
     toDoInput.value ="";
 }
 
-function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null){
-        
-    }
+function something(toDo){
+    console.log(toDo.text);
 }
 
-
+function loadToDos(){
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo) {
+            paintTodo(toDo.text);
+        });        
+    }
+}
 
 function init(){
     loadToDos();
